d27557ddd60f4653cb411af510da20cc353d92d1
dnlknno <danielcardona180478@correo.itm.edu.co>
2021-09-05 16:15:37 -0500

    Add entities and models

---
+++ TimeAzure.Common/Models/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TimeAzure.Common.Models
+{
+    public class Time
+    {
+        public int IdEmpleado { get; set; }
+
+        public DateTime FechaHora { get; set; }
+
+        public int Tipo { get; set; }
+
+        public bool Consolidado { get; set; }
+    }
+}
---
+++ TimeAzure.Functions/Entities/TimeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeAzure.Functions.Entities
+{
+    public class TimeEntity : TableEntity
+    {
+        public int IdEmpleado { get; set; }
+
+        public DateTime FechaHora { get; set; }
+
+        public int Tipo { get; set; }
+
+        public bool Consolidado { get; set; }
+    }
+}
