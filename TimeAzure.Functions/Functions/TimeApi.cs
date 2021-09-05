5016b8db144861659bb42913e8554e6b0760693d
dnlknno <danielcardona180478@correo.itm.edu.co>
2021-09-05 17:34:40 -0500

    Principal functions

---
+++ TimeAzure.Functions/Functions/TimeApi.cs
@@ -0,0 +1,194 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using TimeAzure.Common.Models;
+using TimeAzure.Common.Responses;
+using TimeAzure.Functions.Entities;
+
+namespace TimeAzure.Functions.Functions
+{
+    public static class TimeApi
+    {
+        [FunctionName(nameof(CreateTime))]
+        public static async Task<IActionResult> CreateTime(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "time")] HttpRequest req,
+            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable timeTable,
+            ILogger log)
+        {
+            log.LogInformation("You received the employee's information.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Time time = JsonConvert.DeserializeObject<Time>(requestBody);
+
+            /*if (string.IsNullOrEmpty(time.Tipo))
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = true,
+                    Message = "The request must have a TaskDescription."
+                });
+            }*/
+
+            TimeEntity timeEntity = new TimeEntity
+            {
+                IdEmpleado = time.IdEmpleado,
+                FechaHora = DateTime.UtcNow,
+                Tipo = time.Tipo,
+                Consolidado = false,
+                ETag = "*",
+                PartitionKey = "TIME",
+                RowKey = Guid.NewGuid().ToString()
+            };
+
+            TableOperation addOperation = TableOperation.Insert(timeEntity);
+            await timeTable.ExecuteAsync(addOperation);
+
+            string message = "The employed entry or exit is stored";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = timeEntity
+            }); ;
+        }
+
+        [FunctionName(nameof(UpdateTime))]
+        public static async Task<IActionResult> UpdateTime(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "time/{id}")] HttpRequest req,
+            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable timeTable,
+            string id,
+            ILogger log)
+        {
+            log.LogInformation($"Update received.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Time time = JsonConvert.DeserializeObject<Time>(requestBody);
+
+            //Validate time id
+            TableOperation findOperation = TableOperation.Retrieve<TimeEntity>("TIME", id);
+            TableResult findResult = await timeTable.ExecuteAsync(findOperation);
+            if (findResult.Result == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Todo not found."
+                });
+            }
+
+            //Update time
+            TimeEntity timeEntity = (TimeEntity)findResult.Result;
+            timeEntity.FechaHora = time.FechaHora;
+            timeEntity.Tipo = time.Tipo;
+            /*if (!string.IsNullOrEmpty(todo.TaskDescription))
+            {
+                todoEntity.TaskDescription = todo.TaskDescription;
+            }*/
+
+            TableOperation addOperation = TableOperation.Replace(TimeEntity);
+            await timeTable.ExecuteAsync(addOperation);
+
+            string message = $"Time: {id} updated";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = timeEntity
+            }); ;
+        }
+
+        [FunctionName(nameof(DeleteTime))]
+        public static async Task<IActionResult> DeleteTime(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "time/{id}")] HttpRequest req,
+            [Table("time", "TIME", "{id}", Connection = "AzureWebJobsStorage")] TimeEntity timeEntity,
+            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            string id,
+            ILogger log)
+        {
+            log.LogInformation($"Delete time: {id}, received.");
+
+            if (timeEntity == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Time not found."
+                });
+            }
+
+            await todoTable.ExecuteAsync(TableOperation.Delete(timeEntity));
+
+            string message = $"Time: {timeEntity.RowKey}, deleted.";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = timeEntity
+            });
+        }
+
+        [FunctionName(nameof(GetAllTimes))]
+        public static async Task<IActionResult> GetAllTimes(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "time")] HttpRequest req,
+            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation("Get all inputs and outputs received.");
+
+            TableQuery<TimeEntity> query = new TableQuery<TimeEntity>();
+            TableQuerySegment<TimeEntity> times = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+
+            string message = "Retrieved all inputs and outputs.";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = times
+            });
+        }
+
+        [FunctionName(nameof(GetTimeById))]
+        public static IActionResult GetTimeById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "time/{id}")] HttpRequest req,
+            [Table("time", "TIME", "{id}", Connection = "AzureWebJobsStorage")] TimeEntity timeEntity,
+            string id,
+            ILogger log)
+        {
+            log.LogInformation($"Get time by id:{id}, received.");
+
+            if (timeEntity == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Time not found."
+                });
+            }
+
+            string message = $"Time: {timeEntity.RowKey}, retrieved.";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = timeEntity
+            });
+        }
+    }
+}
